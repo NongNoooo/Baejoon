9946cc66791ce8dd98a0bf4c5b02b2c3c00cb6c7
NongNoooo <83653104+NongNoooo@users.noreply.github.com>
2023-12-05 00:17:26 +0900

    [Bronze V] Title: 사칙연산, Time: 64 ms, Memory: 5520 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+class Program {
+  public static void Main (string[] args) {
+    string[] str = Console.ReadLine().Split();
+    Console.WriteLine(int.Parse(str[0]) + int.Parse(str[1]));
+    Console.WriteLine(int.Parse(str[0]) - int.Parse(str[1]));
+    Console.WriteLine(int.Parse(str[0]) * int.Parse(str[1]));
+    Console.WriteLine(int.Parse(str[0]) / int.Parse(str[1]));
+    Console.WriteLine(int.Parse(str[0]) % int.Parse(str[1]));
+  }
+}
