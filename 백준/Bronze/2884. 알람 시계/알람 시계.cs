7eae63acf0270c5486323ffb33c736aa3f71436f
NongNoooo <83653104+NongNoooo@users.noreply.github.com>
2023-12-05 16:53:44 +0900

    [Bronze III] Title: 알람 시계, Time: 64 ms, Memory: 5532 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string[] time = Console.ReadLine().Split();
+    int h = int.Parse(time[0]);
+
+    int m = int.Parse(time[1]);
+
+    m -= 45;
+
+    if (m < 0){
+      m += 60;
+      h -= 1;
+      if (h < 0) h = 23;
+    }
+
+    Console.WriteLine($"{h} {m}");
+  }
+}
