ccb4e20e80d4534b1203f0a08bf1271d27b1fd21
NongNoooo <83653104+NongNoooo@users.noreply.github.com>
2023-12-12 10:05:15 +0900

    [Bronze V] Title: X보다 작은 수, Time: 76 ms, Memory: 7244 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cs
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cs
@@ -9,11 +9,10 @@ class Program
 
     int[] nums = Array.ConvertAll(Console.ReadLine().Split(), s => int.Parse(s));
 
-    int numbersCount = int.Parse(str[0]);
     int targetNumber = int.Parse(str[1]);
     StringBuilder strBuilder = new StringBuilder();
     
-    for(int i = 0; i < numbersCount; i++)
+    for(int i = 0; i < nums.Length; i++)
     {
       if(nums[i] < targetNumber) strBuilder.Append(nums[i] + " ");
     }
